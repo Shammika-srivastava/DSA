9f87eb58e0ad6c81f819d128c7d8abb162e1190b
shammika srivastava <shammikasrivastava@gmail.com>
2026-07-13 18:37:15 +0530

    Time: 4 ms (24.09%) | Memory: 20.6 MB (42.16%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -3,7 +3,7 @@ class Solution:
         if not nums:
             return 0
         j =0
-        for i in range(len(nums)):
+        for i in range(1,len(nums)):
             if nums[j] != nums[i]:
                 j+= 1
                 nums[j] = nums[i]
