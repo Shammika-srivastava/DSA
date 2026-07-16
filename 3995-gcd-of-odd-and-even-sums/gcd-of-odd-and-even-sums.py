9d4ffbee4215d82e1e84c4333fe603cbb566b95e
shammika srivastava <shammikasrivastava@gmail.com>
2026-07-16 10:26:01 +0530

    Time: 99 ms (6.92%) | Memory: 19.2 MB (53.12%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.py
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.py
@@ -1,3 +1,23 @@
 class Solution:
     def gcdOfOddEvenSums(self, n: int) -> int:
-        return n
+        sumOdd=0
+        sumEven=0
+        count =n
+        i=1
+        while count!=0:
+            if i%2==0:
+                sumEven+=i
+                count-=1
+            i+=1
+        j=1
+        while n!=0:
+            if j%2!=0:
+                sumOdd+=i
+                n-=1    
+            j+=1    
+            
+        
+        while sumEven!=0:
+            sumOdd,sumEven = sumEven, sumOdd%sumEven
+        
+        return sumOdd
