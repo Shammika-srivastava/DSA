f8db048115b890fc626b141f29d2516675665443
shammika srivastava <shammikasrivastava@gmail.com>
2026-07-31 11:51:06 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (83.76%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.py
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.py
@@ -1,10 +1,5 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        if len(word)<=8:
-            return len(word)
-        elif len(word)<=16:
-            return 8 + (2 * len(word[8:]))
-        elif len(word)<=24:
-            return 8+(2*len(word[8:16]))+(3*len(word[16:]))
-        else:
-            return 8+(2*len(word[8:16]))+(3*len(word[16:24]))+(4*len(word[24:]))
+        n = len(word)
+        m = (n - 1) // 8 + 1
+        return m * (m - 1) * 4 + (n - (m - 1) * 8) * m
