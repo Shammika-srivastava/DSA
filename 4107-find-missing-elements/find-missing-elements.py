5735a50f13fbfe8c7c3f35b9f758913249043ed0
shammika srivastava <shammikasrivastava@gmail.com>
2026-08-04 08:34:10 +0530

    Time: 7 ms (24.45%) | Memory: 19.3 MB (18.85%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.py
+++ 4107-find-missing-elements/find-missing-elements.py
@@ -1,13 +1,5 @@
 class Solution:
     def findMissingElements(self, nums: List[int]) -> List[int]:
-        result = 0
-        # XOR all values in the range
-        for i in range(min(nums), max(nums)+1):
-            if i < len(nums):
-                result ^= i ^ nums[i]
-            else:
-                result ^= i
-        # Convert XOR result into a list of missing numbers
         missing = []
         for i in range(min(nums), max(nums)+1):
             if i not in nums:
