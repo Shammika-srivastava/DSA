67e3a746da0dc401723ce6a5cf38bbb351c99a1c
shammika srivastava <shammikasrivastava@gmail.com>
2026-08-19 09:19:05 +0530

    Time: 3 ms (55.26%), Space: 20.4 MB (93.78%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -15,3 +15,17 @@ class Solution:
             else:
                 stack.append(int(num))
         return stack[0]
+        # stack = []
+        # ops = {
+        #     '+': lambda a, b: a + b,
+        #     '-': lambda a, b: a - b,
+        #     '*': lambda a, b: a * b,
+        #     '/': lambda a, b: int(a / b)  # truncate toward 0
+        # }
+        # for token in tokens:
+        #     if token in ops:
+        #         b, a = stack.pop(), stack.pop()
+        #         stack.append(ops[token](a, b))
+        #     else:
+        #         stack.append(int(token))
+        # return stack[0]
