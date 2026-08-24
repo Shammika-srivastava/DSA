75de879c9c292b1dd55af2d20f4bca5506606a87
shammika srivastava <shammikasrivastava@gmail.com>
2026-08-24 20:54:39 +0530

    Time: 503 ms (5.08%), Space: 68.9 MB (5.64%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.py
+++ 0577-employee-bonus/0577-employee-bonus.py
@@ -6,3 +6,10 @@ def employee_bonus(employee: pd.DataFrame, bonus: pd.DataFrame) -> pd.DataFrame:
     final = df[(df['bonus']<1000) | df['bonus'].isnull()] 
     print(final)
     return final[['name','bonus']]
+
+    #     # Merge Employee with Bonus
+    # df = employee.merge(bonus, on="empId", how="left")
+
+    # # Filter condition: bonus < 1000 OR bonus is null
+    # result = df[(df["bonus"].isna()) | (df["bonus"] < 1000)][["name", "bonus"]]
+    # return result
